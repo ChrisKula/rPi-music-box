a2133833116a500a69ff42ccaa5b57c8a203b7f0
Christian KULA <christian.kula.ck@gmail.com>
2019-01-16 17:53:40 +0100

    Create Activity for discovering Music box

---
+++ rPi-music-box-remote/src/main/java/com/christiankula/rpimusicbox/remote/musicboxdiscovery/ui/MusicBoxDiscoveryActivity.kt
@@ -0,0 +1,13 @@
+package com.christiankula.rpimusicbox.remote.musicboxdiscovery.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.christiankula.rpimusicbox.remote.R
+
+class MusicBoxDiscoveryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_box_discovery)
+    }
+}
