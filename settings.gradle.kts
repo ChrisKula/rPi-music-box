7230f9ac4d435d4ac9c51f78d195f08933a86aa9
Christian KULA <christian.kula.ck@gmail.com>
2019-06-21 13:12:35 +0200

    Ensure that each module's Gradle build file is correctly named

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,8 @@ include(":rPi-music-box-remote")
 include(":rxnearby")
 include(":androidcommons")
 include(":commons")
+
+// Ensure that each module's Gradle build file name is formatted as <module-name>.gradle.kts
+rootProject.children.forEach { module ->
+    module.buildFileName = "${module.name}.gradle.kts"
+}
