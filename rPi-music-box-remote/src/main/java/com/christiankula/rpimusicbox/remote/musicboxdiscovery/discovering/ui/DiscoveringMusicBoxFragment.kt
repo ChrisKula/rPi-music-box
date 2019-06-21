708f83a685268c8c9a6d2b50c96767dfc052203f
Christian KULA <christian.kula.ck@gmail.com>
2019-06-21 12:47:19 +0200

    Migrate 'rpi-musicbox-remote' module to Android X

--- rPi-music-box-remote/src/main/java/com/christiankula/rpimusicbox/remote/musicboxdiscovery/discovering/ui/DiscoveringMusicBoxFragment.kt
+++ rPi-music-box-remote/src/main/java/com/christiankula/rpimusicbox/remote/musicboxdiscovery/discovering/ui/DiscoveringMusicBoxFragment.kt
@@ -2,11 +2,11 @@ package com.christiankula.rpimusicbox.remote.musicboxdiscovery.discovering.ui
 
 import android.content.Context
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.christiankula.rpimusicbox.remote.R
 import kotlinx.android.synthetic.main.fragment_discovering_music_box.*
 
