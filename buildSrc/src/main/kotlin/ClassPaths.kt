7af0a21c25f383303fbc3cf1f4edf7e102313f15
Christian KULA <christian.kula.ck@gmail.com>
2019-06-21 11:02:24 +0200

    Create root 'buildSrc' directory for Gradle Kotlin DSL files

---
+++ buildSrc/src/main/kotlin/ClassPaths.kt
@@ -0,0 +1,4 @@
+object ClassPaths {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.gradlePlugin}"
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+}
---
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -0,0 +1,17 @@
+object Plugins {
+
+    // Android plugins
+    const val androidApplication = "com.android.application"
+    const val androidFeature = "com.android.feature"
+    const val androidLibrary = "com.android.library"
+
+    // Kotlin plugins
+    // Usage: Replace `apply plugin: 'kotlin-X'` by `kotlin(X)`
+    const val kotlin = "jvm"
+    const val kotlinAndroid = "android"
+    const val kotlinExtensions = "android.extensions"
+    const val kapt = "kapt"
+
+    // Standard Java library
+    const val javaLib = "java-library"
+}
