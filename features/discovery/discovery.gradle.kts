6aac14908f4335834c5f64a7ace186b2615f9a46
Christian KULA <christian.kula.ck@gmail.com>
2019-07-05 11:43:14 +0200

    Use 'design' module's values in 'discovery' module

--- features/discovery/discovery.gradle.kts
+++ features/discovery/discovery.gradle.kts
@@ -17,6 +17,8 @@ androidExtensions {
 }
 
 dependencies {
+    implementation(project(":core:design"))
+
     implementation(project(":libraries:androidcommons"))
     api(project(":libraries:rxnearby"))
     api(project(":libraries:permissions"))
