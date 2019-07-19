2e9a85dd556f137896aaf7060120938498239727
Christian KULA <christian.kula.ck@gmail.com>
2019-07-19 14:48:56 +0200

    Use SafeArgs in FoundMusicBoxFragment

--- features/discovery/src/main/java/rpimusicbox/features/discovery/ui/found/FoundMusicBoxFragment.kt
+++ features/discovery/src/main/java/rpimusicbox/features/discovery/ui/found/FoundMusicBoxFragment.kt
@@ -6,21 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_found_music_box.*
 import rpimusicbox.features.discovery.R
 import rpimusicbox.features.discovery.ui.MusicBoxDiscoveryViewModel
-
-private const val FOUND_MUSIC_BOX_NAME_KEY = "FOUND_MUSIC_BOX_NAME"
 
 internal class FoundMusicBoxFragment : Fragment() {
 
     private lateinit var viewModel: MusicBoxDiscoveryViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        validateBundle(savedInstanceState)
-    }
+    private val navArgs: FoundMusicBoxFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_found_music_box, container, false)
@@ -31,18 +26,12 @@ internal class FoundMusicBoxFragment : Fragment() {
 
         connectToMusicBoxButton.setOnClickListener { viewModel.onConnectToMusicBoxButtonClicked() }
 
-        foundMusicBoxName.text = arguments!!.getString(FOUND_MUSIC_BOX_NAME_KEY)
+        foundMusicBoxName.text = navArgs.foundMusicBoxName
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         viewModel = ViewModelProviders.of(requireActivity()).get(MusicBoxDiscoveryViewModel::class.java)
-    }
-
-    private fun validateBundle(bundle: Bundle?) {
-        if (bundle != null && bundle.getString(FOUND_MUSIC_BOX_NAME_KEY).isNullOrEmpty()) {
-            throw IllegalStateException()
-        }
     }
 }
